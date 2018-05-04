7e16dd41f023f1cb777a41c6ad8ffee4a47e50cf
pmukhin <pauri.lentonen@gmail.com>
2018-05-04 23:30:40 +0700

    Base object system with Int, Float, String, Rune types

---
+++ pkg/object/object.go
@@ -0,0 +1,66 @@
+package object
+
+import "fmt"
+
+type Type int8
+
+const (
+	TInt      Type = iota
+	TFunction
+	TString
+	TRune
+	TFloat
+)
+
+var type2str = map[Type]string{
+	TInt:      "TInt",
+	TFunction: "TFunction",
+	TString:   "TString",
+	TRune:     "TRune",
+	TFloat:    "TFloat",
+}
+
+func (t Type) String() string {
+	return type2str[t]
+}
+
+type Object interface {
+	String() string
+	Type() Type
+}
+
+type Float struct {
+	Value float64
+}
+
+func (f Float) String() string {
+	return fmt.Sprintf("%f", f.Value)
+}
+
+func (f Float) Type() Type {
+	return TFloat
+}
+
+type Int struct {
+	Value int64
+}
+
+func (i Int) String() string {
+	return fmt.Sprintf("%d", i.Value)
+}
+
+func (Int) Type() Type {
+	return TInt
+}
+
+type String struct {
+	Value string
+}
+
+func (s String) String() string {
+	return fmt.Sprintf("%s", s.Value)
+}
+
+func (String) Type() Type {
+	return TString
+}
